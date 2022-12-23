6ae629806ff0df182d22802837ceef28bb4b8750
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-12-23 12:04:25 +0530

    updated routers

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NodeJS-CRUD-API
 
 #Dependencies <br>
-1.npm install --save-dev nodemon
+1.npm install --save-dev nodemon <br>
+2.npm install uuid
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,35 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "John",
-        lastName: "Doe",
-        age: 23,
-        preferLanguage: "Java"
-    },
-    {
-        firstName: "Sia",
-        lastName: "Shim",
-        age: 21,
-        preferLanguage: "React"
-    }
-]
+const users = [];
 
 router.get('/', (req, res) => {
-    console.log(users);
-
     res.send(users);
 });
 
 router.post('/', (req, res) => {
+    const user = req.body;
+
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+router.get('/:id', (req,res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id == id);
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
     
+    users = users.filter((user) => true);
 });
 
 export default router;
